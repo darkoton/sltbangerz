fca704d5f8b0c5c361de66027590c4ffd08da18b
darkoto12 <darkoto12@gmail.com>
2025-02-04 15:59:16 +0200

    build

--- gulp/tasks/js.js
+++
@@ -1,8 +0,0 @@
-import webpack from "webpack-stream"
-
-export const js = () => {
-  return app.gulp
-  .src(`${app.path.srcFolder}/js/**/*.js`)
-  .pipe(app.gulp.dest(`${app.path.buildFolder}/js`))
-  .pipe(app.plugins.browsersync.stream());
-}
--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-  app.plugins.browsersync.init({
-    server: {
-      baseDir: `${app.path.buildFolder}`
-    },
-    notify: false,
-    port: 3000
-  })
-}
