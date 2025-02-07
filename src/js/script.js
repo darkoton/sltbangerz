f0e84a869602a6a092ff858ff61667a67b048c69
darkoto12 <darkoto12@gmail.com>
2025-02-07 18:59:21 +0200

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,21 @@
 
 @@include("./modules/device.js"); //DEFINE DEVICE
 
-/* @@include("./modules/preloader.js"); */ // PRELOADER
+/* @@include("./modules/preloader.js"); // PRELOADER */ 
 
-/* @@include("./modules/dynamic_adap.js") */  // DYNAMIC ADAPTIVE
+/* @@include("./modules/dynamic_adap.js")  // DYNAMIC ADAPTIVE */ 
 
-/* @@include("./modules/scroll_header.js") */  // SCROLL HEADER
+/* @@include("./modules/scroll_header.js")  // SCROLL HEADER */ 
 
 @@include("./modules/swiper.js"); // SLIDER SWIPER
 
-/* @@include "./modules/animate_scroll.js" */  // ANIMATE WITH SCROLL
+/* @@include "./modules/animate_scroll.js" // ANIMATE WITH SCROLL */  
 
 @@include("./modules/tabs.js"); // TABS
 
-/* @@include("./modules/parallax.js") */  // PARALLAX EFFECT
+@@include("./modules/spoiler.js"); // TABS
+
+/* @@include("./modules/parallax.js")  // PARALLAX EFFECT */
 
 //< " СКРИПТЫ " >=============================================================================================================>//
 
